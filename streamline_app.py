63b32b49c57f60078c559f317645611b75a5d928
tkbandaru <tbandaru@albany.edu>
2025-03-28 13:13:42 -0400

    Update streamline_app.py
    session changes

--- streamline_app.py
+++ streamline_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 helpful_links = [
@@ -21,7 +21,9 @@ st.write('Choose the fruits you want in your custom Smoothie')
 name_on_order = st.text_input('name on smoothie')
 #st.write(name_on_order)
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
+#session = get_active_session()
 my_df = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col('FRUIT_NAME'))
 #st.dataframe(data = my_df, use_container_width=True)
 
